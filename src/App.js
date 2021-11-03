cb8169e6daadb20115ad7309ef51ff9055fd90bd
Bikash Shaw <1985.bikash.shaw@gmail.com>
2021-11-03 22:41:18 +0530

    Reverting code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <button className="btn btn-primary">Save Bikash</button>
+      <button className="btn btn-primary">Save</button>
     </div>
   );
 }
